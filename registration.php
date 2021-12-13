d8ba6b30c03cbf7bb52fe004922cf2f57e498dd0
João Victor Pereira <vpjoao98@gmail.com>
2021-12-13 16:43:42 -0300

    feat: adding module Schatzmann_Base

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright Copyright © 2021 - Schatzmann. All rights reserved.
+ * @author João Victor Pereira <vpjoao98@gmail.com>
+ * @package Base
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Schatzmann_Base', __DIR__);
